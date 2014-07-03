5e28185afadb5b4a5921036d94a217db5c3fb22b
Maciej Małecki <me@mmalecki.com>
2014-07-03 02:45:22 +0200

    Do not return an error in the example client

--- example/lb-client.js
+++ example/lb-client.js
@@ -2,18 +2,18 @@ require('seneca')()
   .use(require('../'), {
     workers:[
       { type: 'tcp', port: 9000 },   // same options you would pass normally to a transport
-      { type: 'tcp', port: 9001 },
-      { type: 'http', port: 9002 }, // transports do not have to be of same type
+      { type: 'tcp', port: 9001 }
     ]
   })
-  .client({ type: 'loadbalance' })
   .ready(function(){
     var seneca = this
 
     // this should round robin
+    console.log('ready')
     setInterval(function () {
       seneca.act({ a: 1 }, function (err, out) {
         console.dir(arguments)
       })
     }, 222)
   })
+  .client({ type: 'loadbalance-transport' })
