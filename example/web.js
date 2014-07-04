640607da6abfc5ac8eaa877c1a303486c0f6fa55
Maciej Małecki <me@mmalecki.com>
2014-07-04 18:33:44 +0200

    Add a `seneca-web` integration example

---
+++ example/web.js
@@ -0,0 +1,32 @@
+var express = require('express')
+
+var webPort = parseInt(process.argv[2], 10)
+
+var seneca = require('seneca')()
+seneca
+  .use(require('../'), {
+    workers:[
+      { type: 'tcp', port: 9000 },   // same options you would pass normally to a transport
+      { type: 'tcp', port: 9001 }
+    ]
+  })
+  .use(require('seneca-web'))
+  .ready(function () {
+    seneca.act('role:web', {
+      use: {
+        prefix: '/loadbalance',
+        pin: { role: 'loadbalance', cmd: '*' },
+        map: {
+          add: { POST: true },
+          list: { GET: true },
+          close: { POST: true }
+        }
+      }
+    })
+
+    var app = express()
+    console.dir(seneca.export('web').toString())
+    app.use(seneca.export('web'))
+    app.use(require('connect-query'))
+    app.listen(webPort)
+  })
