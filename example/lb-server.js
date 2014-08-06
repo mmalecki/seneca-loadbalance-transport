a887126d96ad11adb73106894176fc30ffb02203
Maciej Małecki <me@mmalecki.com>
2014-08-06 21:13:32 +0200

    Check CLI parameters and print debug info

--- example/lb-server.js
+++ example/lb-server.js
@@ -1,10 +1,16 @@
 var type = process.argv[2]
 var port = parseInt(process.argv[3], 10)
 
+if (!type || !port) {
+  console.error('usage: lb-server.js <type> <port>')
+  process.exit(1)
+}
+
 var seneca = require('seneca')()
 seneca
   .use(function () {
     this.add({ a: 1 }, function (args, done) {
+      console.log('got request')
       done(null, { b: 1 + args.a })
     })
     this.add({ role: 'transport', cmd: 'ping' }, function (args, cb) {
